d7130e7f512ccff518f4d13326314f543a5acdc1
wjdtlr0822 <a01055687052@gmail.com>
2025-09-01 00:11:34 +0900

    sql join, group by add

--- sql2.py
+++ sql2.py
@@ -10,3 +10,26 @@ for row in rows:
     print(row)
 
 conn.close()
+
+###원격서버에 연결하기 위해서###
+#import psycopg2
+#
+#conn = psycopg2.connect(
+#    host="127.0.0.1",
+#    user="postgres",
+#    password="1234",
+#    dbname="testdb"
+#)
+###
+
+### GROUP BY 예시
+#SELECT survived, COUNT(*) 
+#FROM passengers 
+#GROUP BY survived;
+###
+
+### JOIN 예시
+#SELECT u.name, o.amount
+#FROM users u
+#JOIN orders o ON u.id = o.user_id;
+###
